d5ccb19ed2776e8774b92ee3b872468235e7ed6e
Pedro Bergaglio <pedrobergaglio04@gmail.com>
2023-08-15 22:50:02 -0300

    titulo video y otros h2

--- components/hero.tsx
+++ components/hero.tsx
@@ -33,11 +33,10 @@ export default function Hero() {
 
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
-            <h1 className="h1 mb-4" data-aos="fade-up" >CAMBIÁ EL CHIP <br />
-                                                       TRAE TU EMPRESA AL PRESENTE
+            <h1 className="h1 mb-4 tracking-[0.25em] text-7xl" data-aos="fade-up" >CAMBIÁ EL CHIP
                                                                      </h1>
-            <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">Tu socio de confianza en soluciones a medida para PyMEs y empresas de todos los tamaños.</p>
-            <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center">
+            <p className="text-3xl text-gray-400 mb-10" data-aos="fade-up" data-aos-delay="200">CONTROL Y GESTIÓN DESDE DÓNDE ESTÉS </p>
+            <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center tracking-wider">
               {/* Contactanos */}
              {/*  <div data-aos="fade-up" data-aos-delay="400">
                 <a className="btn text-white bg-blue-900 hover:bg-blue-800 w-full mb-4 sm:w-auto sm:mb-0"  >Contactanos</a>
