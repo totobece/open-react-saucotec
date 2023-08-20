99d02b8f093f51b60af90fbf15b0068089b508b3
Pedro Bergaglio <pedrobergaglio04@gmail.com>
2023-08-19 22:13:06 -0300

    zig zag, rounding, etc

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import VideoThumb from '@/public/images/hero-image-01.jpg'
+import VideoThumb from '@/public/images/hero-image-01.png'
 import ModalVideo from '@/components/modal-video'
 import * as Scroll from 'react-scroll';
 import { Link, Button, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
@@ -16,8 +16,8 @@ export default function Hero() {
       <div className="max-w-6xl mx-auto px-4 sm:px-6 relative">
 
         {/* Illustration behind hero content */}
-        <div className="absolute left-0 bottom-0 -ml-20 hidden lg:block pointer-events-none" aria-hidden="true" data-aos="fade-up" data-aos-delay="400">
-          <svg className="max-w-full" width="564" height="552" viewBox="0 0 564 552" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <div className="absolute left-0 bottom-0  -ml-20 hidden lg:block pointer-events-none" aria-hidden="true" data-aos="fade-up" data-aos-delay="400">
+          <svg className="max-w-full " width="564" height="552" viewBox="0 0 564 552" fill="none" xmlns="http://www.w3.org/2000/svg">
             <defs>
               <linearGradient id="illustration-02" x1="-3.766" y1="300.204" x2="284.352" y2="577.921" gradientUnits="userSpaceOnUse">
                 <stop stopColor="#5D5DFF" stopOpacity=".01" />
@@ -29,15 +29,14 @@ export default function Hero() {
         </div>
 
         {/* Hero content */}
-        <div className="relative pt-32 pb-10 md:pt-40 md:pb-16">
+        <div className="relative pt-32 pb-10 md:pt-40 md:pb-16 ">
 
           {/* Section header */}
-          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
-            <h1 className="text-6xl h1 mb-4" data-aos="fade-up" >CAMBIÁ EL CHIP <br />
-                                                       TRAE TU EMPRESA AL PRESENTE
+          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16 ">
+            <h1 className="text-9xl h1 mb-4 tracking-[0.09em]" data-aos="fade-up" >CAMBIÁ EL CHIP
                                                                      </h1>
-            <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">Tu socio de confianza en soluciones a medida para PyMEs y empresas de todos los tamaños.</p>
-            <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center">
+            <p className="text-3xl text-gray-400 mb-8 tracking-[0.15em]" data-aos="fade-up" data-aos-delay="200">control y gestión desde donde estés</p>
+            <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center ">
               {/* Contactanos */}
              {/*  <div data-aos="fade-up" data-aos-delay="400">
                 <a className="btn text-white bg-blue-900 hover:bg-blue-800 w-full mb-4 sm:w-auto sm:mb-0"  >Contactanos</a>
--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -8,12 +8,12 @@ export default function Header() {
   return (
     <header className="absolute w-full z-30">
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
-        <div className="flex items-center justify-between h-20">
+        <div className="flex items-center justify-between h-40">
           {/* Site branding */}
           <div className="shrink-0 mr-4">
             {/* Logo */}
             <Link href="/" className="block" aria-label="Cruip">
-            <div className='w-32 h-10 fill-current '>
+            <div className='w-64 h-20 fill-current '>
                     <Image
                       priority
                       src={SaucotecLogo}
