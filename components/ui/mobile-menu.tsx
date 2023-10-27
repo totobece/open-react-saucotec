c0b8ee39db0c9ba67f06d62b9fa4ef9ed74561c6
TOBIAS BECERRA <savaltobias5@gmail.com>
2023-10-27 13:28:43 -0300

    wpp button mobile menu

--- components/ui/mobile-menu.tsx
+++ components/ui/mobile-menu.tsx
@@ -2,7 +2,8 @@
 
 import { useState, useRef, useEffect } from 'react'
 import Link from 'next/link'
-import WPPlogo from "../../public/images/whatsapp-svgrepo-com.svg"
+import WPPlogo from "../../public/images/Free whatsapp white logo vector.svg.svg"
+import Image from 'next/image'
 
 export default function MobileMenu() {
   const [mobileNavOpen, setMobileNavOpen] = useState<boolean>(false)
@@ -33,49 +34,26 @@ export default function MobileMenu() {
 
   return (
     <div className="md:hidden">
-    {/* Hamburger button */}
-    <button
-      ref={trigger}
-      className={`hamburger ${mobileNavOpen && 'active'}`}
-      aria-controls="mobile-nav"
-      aria-expanded={mobileNavOpen}
-      onClick={() => setMobileNavOpen(!mobileNavOpen)}
-    >
-      <span className="sr-only">Menu</span>
-      <svg
-        className="w-6 h-6 fill-current text-gray-300 hover:text-gray-200 transition duration-150 ease-in-out"
-        viewBox="0 0 24 24"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <rect y="4" width="24" height="2" rx="1" />
-        <rect y="11" width="24" height="2" rx="1" />
-        <rect y="18" width="24" height="2" rx="1" />
-      </svg>
-    </button>
+              <ul className="flex grow justify-end flex-wrap items-center">
+                  <li>
+                  <Link href="https://api.whatsapp.com/send?phone=1170135056&text=Hola,%20tengo%20una%20consulta." 
+                  className="btn-sm text-white bg-blue-888 hover:bg-blue-888 ml-3 flex items-center" style={{transform: 'translateY(-11px)'}}>
 
-    {/*Mobile navigation */}
-    <nav
-      id="mobile-nav"
-      ref={mobileNav}
-      className="absolute top-full z-20 left-0 w-full px-4 sm:px-6 overflow-hidden transition-all duration-300 ease-in-out"
-      style={mobileNavOpen ? { maxHeight: mobileNav.current?.scrollHeight, opacity: 1 } : { maxHeight: 0, opacity: 0.8 }}
-    >
-      <ul className="bg-gray-800 px-4 py-2">
-        <li>
-         
-        </li>
-        <li>
-          <Link
-            href="https://api.whatsapp.com/send?phone=1170135056&text=Hola,%20tengo%20una%20consulta."
-            className="font-medium w-full inline-flex items-center justify-center border border-transparent px-4 py-2 my-2 rounded-sm text-white bg-blue-700 hover:bg-blue-700 transition duration-150 ease-in-out" onClick={() => setMobileNavOpen(false)}
-          >
-          
-           Contactanos
-           
-          </Link>
-        </li>
-      </ul>
-    </nav>
+                    <Image
+                    src={WPPlogo}
+                    alt='Whatsapp Logo'
+                    className='w-5 h-5 mr-2'
+                    style={{ transform: 'translateX(3px)' }} 
+                    />
+
+                    
+                  </Link>
+                </li>
+
+
+            </ul>
+
+    
   </div>
   )
 }
