0b427e105d6eed515a6ae83cb31a6efd0a6742c8
Pedro Bergaglio <pedrobergaglio04@gmail.com>
2023-11-08 00:14:46 -0300

    casi finished

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -13,41 +13,37 @@ export default function Testimonials() {
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
             <h2 className="h2 mb-4">no tomes nuestra palabra</h2>
-            <p className="text-xl text-gray-800">Conocé lo que piensan quienes trabajaron con nosotros</p>
+            <p className="text-xl text-gray-800">lo que piensan quienes trabajaron con nosotros</p>
           </div>
 
           {/* Testimonials */}
           <div className="max-w-sm mx-auto grid gap-8 lg:grid-cols-3 lg:gap-6 items-start lg:max-w-none">
-
-            {/* 1st testimonial */}
-            <div className="flex flex-col h-full p-6 bg-blue-888 rounded-2xl" data-aos="fade-up">
-              <div>
-                <div className="relative inline-flex flex-col mb-4">
-                  <Image className="rounded-full" src={TestimonialImage01} width={48} height={48} alt="Testimonial 01" />
-                  <svg className="absolute top-0 right-0 -mr-3 w-6 h-5 fill-current text-blue-600" viewBox="0 0 24 20" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M0 13.517c0-2.346.611-4.774 1.833-7.283C3.056 3.726 4.733 1.648 6.865 0L11 2.696C9.726 4.393 8.777 6.109 8.152 7.844c-.624 1.735-.936 3.589-.936 5.56v4.644H0v-4.531zm13 0c0-2.346.611-4.774 1.833-7.283 1.223-2.508 2.9-4.586 5.032-6.234L24 2.696c-1.274 1.697-2.223 3.413-2.848 5.148-.624 1.735-.936 3.589-.936 5.56v4.644H13v-4.531z" />
-                  </svg>
-                </div>
-              </div>
-              <blockquote className="text-lg text-gray-400 grow">Nort Revestimientos...</blockquote>
-              <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
-                <cite className="text-gray-200 not-italic">Cesar Cabrera</cite> - <a className="text-blue-600 hover:text-gray-200 transition duration-150 ease-in-out" href="#0">Nort Revestimientos</a>
-              </div>
-            </div>
 
             {/* 2nd testimonial */}
             <div className="flex flex-col h-full p-6 bg-blue-888 rounded-2xl" data-aos="fade-up" data-aos-delay="200">
               <div>
                 <div className="relative inline-flex flex-col mb-4">
                   <Image className="rounded-full" src={TestimonialImage02} width={48} height={48} alt="Testimonial 02" />
-                  <svg className="absolute top-0 right-0 -mr-3 w-6 h-5 fill-current text-blue-600" viewBox="0 0 24 20" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M0 13.517c0-2.346.611-4.774 1.833-7.283C3.056 3.726 4.733 1.648 6.865 0L11 2.696C9.726 4.393 8.777 6.109 8.152 7.844c-.624 1.735-.936 3.589-.936 5.56v4.644H0v-4.531zm13 0c0-2.346.611-4.774 1.833-7.283 1.223-2.508 2.9-4.586 5.032-6.234L24 2.696c-1.274 1.697-2.223 3.413-2.848 5.148-.624 1.735-.936 3.589-.936 5.56v4.644H13v-4.531z" />
-                  </svg>
+                  
                 </div>
               </div>
-              <blockquote className="text-lg text-gray-400 grow">— Energía???????????????</blockquote>
+              <blockquote className="text-lg text-gray-200 tracking-[0.05em] grow italic" style={{ fontFamily: 'helvetica, sans-serif' }}>Energía Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </blockquote>
               <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
-                <cite className="text-gray-200 not-italic">Nicolas Albertoni</cite> - <a className="text-blue-600 hover:text-gray-200 transition duration-150 ease-in-out" href="#0">Energía Global</a>
+                <cite className="text-gray-200 not-italic">Nicolas Albertoni</cite> - <a className="text-blue-600 hover:text-gray-200 transition duration-150 ease-in-out" href="https://www.energiaglobal.com.ar/" target="_blank">Energía Global</a>
+              </div>
+            </div>
+
+            {/* 1st testimonial */}
+            <div className="flex flex-col h-full p-6 bg-blue-888 rounded-2xl" data-aos="fade-up">
+              <div>
+                <div className="relative inline-flex flex-col mb-4">
+                  <Image className="rounded-full" src={TestimonialImage01} width={48} height={48} alt="Testimonial 01" />
+                  
+                </div>
+              </div>
+              <blockquote className="text-lg text-gray-200 tracking-[0.05em] grow italic" style={{ fontFamily: 'helvetica, sans-serif' }}>Los resultados que obtuvimos al trabajar con SaucoTec son excelentes. <br></br>La diferencia está en que todo está hecho a la medida de nuestra empresa. Sabemos que ante cualquier necesidad, ellos están ahí para ayudarnos.</blockquote>
+              <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
+                <cite className="text-gray-200 not-italic">Cesar Cabrera</cite> - <a className="text-blue-600 hover:text-slate-50 transition duration-150 ease-in-out" href="https://www.nort-online.com/" target="_blank">Nort Revestimientos</a>
               </div>
             </div>
 
@@ -56,14 +52,12 @@ export default function Testimonials() {
               <div>
                 <div className="relative inline-flex flex-col mb-4">
                   <Image className="rounded-full" src={TestimonialImage03} width={48} height={48} alt="Testimonial 03" />
-                  <svg className="absolute top-0 right-0 -mr-3 w-6 h-5 fill-current text-blue-600" viewBox="0 0 24 20" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M0 13.517c0-2.346.611-4.774 1.833-7.283C3.056 3.726 4.733 1.648 6.865 0L11 2.696C9.726 4.393 8.777 6.109 8.152 7.844c-.624 1.735-.936 3.589-.936 5.56v4.644H0v-4.531zm13 0c0-2.346.611-4.774 1.833-7.283 1.223-2.508 2.9-4.586 5.032-6.234L24 2.696c-1.274 1.697-2.223 3.413-2.848 5.148-.624 1.735-.936 3.589-.936 5.56v4.644H13v-4.531z" />
-                  </svg>
+                  
                 </div>
               </div>
-              <blockquote className="text-lg text-gray-400 grow">politap pedro rossi</blockquote>
+              <blockquote className="text-lg text-gray-200 tracking-[0.05em] grow italic" style={{ fontFamily: 'helvetica, sans-serif' }}>En Politap teníamos un déficit en la accesibilidad y eficiencia del proceso de venta. Desde SaucoTec supieron cómo desarrollar una solución que se integre perfectamente a nuestro sistema logrando agilizar nuestras operaciones.</blockquote>
               <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
-                <cite className="text-gray-200 not-italic">Pedro Rossi</cite> - <a className="text-blue-600 hover:text-gray-200 transition duration-150 ease-in-out" href="#0">PoliTap</a>
+                <cite className="text-gray-200 not-italic">Pedro Rossi</cite> - <a className="text-blue-600 hover:text-gray-200 transition duration-150 ease-in-out" href="/">PoliTap</a>
               </div>
             </div>
 
