691502b564443f456ef6af6efd92388a171e2d8c
totobece <savaltobias5@gmail.com>
2023-12-05 15:50:22 -0300

    windows.gtag por ahora afuera, era de cookies

--- lib/gtagHelper.ts
+++ lib/gtagHelper.ts
@@ -1,9 +1,9 @@
-export const pageview = (GA_MEASUREMENT_ID : string, url : string) => {
+// export const pageview = (GA_MEASUREMENT_ID : string, url : string) => {
     
-    if(typeof window !== 'undefined'){
-    window.gtag("config", GA_MEASUREMENT_ID, {
-        page_path: url,
-    }); 
+//     if(typeof window !== 'undefined'){
+//     window.gtag("config", GA_MEASUREMENT_ID, {
+//         page_path: url,
+//     }); 
 
-}
-};
+// }
+// };
