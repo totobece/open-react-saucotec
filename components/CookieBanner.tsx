2952b7d314d6a37f76fbbf795187d0bdba9c8803
totobece <savaltobias5@gmail.com>
2023-12-07 19:00:42 -0300

    gtag code ok, falta getlocalstorage cookies

--- components/CookieBanner.tsx
+++ components/CookieBanner.tsx
@@ -1,57 +1,57 @@
-// 'use client'
+'use client'
 
-// import Link from 'next/link';
-// import { getLocalStorage, setLocalStorage } from '@/lib/storageHelper';
-// import { useState, useEffect } from 'react';
+import Link from 'next/link';
+import { getLocalStorage, setLocalStorage } from '@/lib/storageHelper';
+import { useState, useEffect } from 'react';
 
 
-// export default function CookieBanner() {
+export default function CookieBanner() {
 
-//     const [cookieConsent, setCookieConsent] = useState(false);
+    const [cookieConsent, setCookieConsent] = useState(false);
 
-//     useEffect (() => {
-//         const storedCookieConsent = getLocalStorage("cookie_consent", null)
+    useEffect (() => {
+        const storedCookieConsent = getLocalStorage("cookie_consent", null)
 
-//         setCookieConsent(storedCookieConsent)
-//     }, [setCookieConsent])
+        setCookieConsent(storedCookieConsent)
+    }, [setCookieConsent])
 
     
-//     useEffect(() => {
-//         const newValue = cookieConsent ? 'granted' : 'denied'
+    useEffect(() => {
+        const newValue = cookieConsent ? 'granted' : 'denied'
 
-//         window.gtag("consent", 'update', {
-//             'analytics_storage': newValue
-//         });
+        window.gtag("consent", 'update', {
+            'analytics_storage': newValue
+        });
 
-//         setLocalStorage("cookie_consent", cookieConsent)
+        setLocalStorage("cookie_consent", cookieConsent)
 
-//         //For Testing
-//         console.log("Cookie Consent: ", cookieConsent)
+        //For Testing
+        console.log("Cookie Consent: ", cookieConsent)
 
-//     }, [cookieConsent]);
-
-
-//   return (
-//     <div className={`my-10 mx-auto max-w-max md:max-w-screen-sm
-//                         fixed bottom-0 left-0 right-0 
-//                         flex px-3 md:px-4 py-3 justify-between items-center flex-col sm:flex-row gap-4  
-//                          bg-blue-888 rounded-lg shadow`}>
-//       <div className='text-center'>
-//   <Link href="#">
-//     <p className='text-white'>
-//       We use{''}
-//       <span className='text-blue-600 font-bold ml-2 mr-2'>cookies</span>
-//       {''}on our site.
-//     </p>
-//   </Link>
-// </div>
+    }, [cookieConsent]);
 
 
+  return (
+    <div className={`my-10 mx-auto max-w-max md:max-w-screen-sm
+                        fixed bottom-0 left-0 right-0 
+                        flex px-3 md:px-4 py-3 justify-between items-center flex-col sm:flex-row gap-4  
+                         bg-blue-888 rounded-lg shadow`}>
+      <div className='text-center'>
+  <Link href="#">
+    <p className='text-white'>
+      We use{''}
+      <span className='text-blue-600 font-bold ml-2 mr-2'>cookies</span>
+      {''}on our site.
+    </p>
+  </Link>
+</div>
 
-//       <div className='flex gap-2'>
-//         <button className='px-5 py-2 text-white rounded-md border-blue-888' onClick={() => setCookieConsent(false)}>Decline</button>
-//         <button className='bg-blue-600 px-5 py-2 text-white rounded-lg' onClick={() => setCookieConsent(true)}>Allow Cookies</button>
-//       </div>
-//     </div>
-//   );
-// }
+
+
+      <div className='flex gap-2'>
+        <button className='px-5 py-2 text-white rounded-md border-blue-888' onClick={() => setCookieConsent(false)}>Decline</button>
+        <button className='bg-blue-600 px-5 py-2 text-white rounded-lg' onClick={() => setCookieConsent(true)}>Allow Cookies</button>
+      </div>
+    </div>
+  );
+}
