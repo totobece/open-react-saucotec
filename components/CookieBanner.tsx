9d2cd4dcbde7271d17d5559f2c95f1a1f9af7d7f
totobece <savaltobias5@gmail.com>
2023-12-07 20:01:04 -0300

    handlecookieconsent arreglado, no funcaba bien

--- components/CookieBanner.tsx
+++ components/CookieBanner.tsx
@@ -1,43 +1,51 @@
 'use client'
+
 import Link from 'next/link';
 import { getLocalStorage, setLocalStorage } from '@/lib/storageHelper';
 import { useState, useEffect } from 'react';
 
+
 export default function CookieBanner() {
+
     const [cookieConsent, setCookieConsent] = useState(false);
 
-    useEffect(() => {
-        const storedCookieConsent = getLocalStorage("cookie_consent", null);
-        setCookieConsent(storedCookieConsent);
-    }, [setCookieConsent]);
+    useEffect (() => {
+        const storedCookieConsent = getLocalStorage("cookie_consent", null)
 
+        setCookieConsent(storedCookieConsent)
+    }, [setCookieConsent])
+
+    
     useEffect(() => {
-        const newValue = cookieConsent ? 'granted' : 'denied';
+        const newValue = cookieConsent ? 'granted' : 'denied'
 
         window.gtag("consent", 'update', {
             'analytics_storage': newValue
         });
 
-        setLocalStorage("cookie_consent", cookieConsent);
+        setLocalStorage("cookie_consent", cookieConsent)
 
-        // For Testing
-        console.log("Cookie Consent: ", cookieConsent);
+        //For Testing
+        console.log("Cookie Consent: ", cookieConsent)
+
     }, [cookieConsent]);
 
     const handleDecline = () => {
-        setCookieConsent(true);
+        setCookieConsent(false);
     };
-
+    
     const handleAllowCookies = () => {
         setCookieConsent(true);
     };
+    
 
-    // Renderizar el banner solo si cookieConsent es false
-    return !cookieConsent ? (
+
+    return (
         <div className={`my-10 mx-auto max-w-max md:max-w-screen-sm
-                            fixed bottom-0 left-0 right-0 
-                            flex px-3 md:px-4 py-3 justify-between items-center flex-col sm:flex-row gap-4  
-                             bg-blue-888 rounded-lg shadow`}
+                        fixed bottom-0 left-0 right-0 
+                        ${!cookieConsent ? "flex" : "hidden"} 
+                        px-3 md:px-4 py-3 justify-between items-center flex-col sm:flex-row gap-4  
+                        bg-blue-888 rounded-lg shadow`}
         >
             <div className='text-center'>
                 <Link href="#">
@@ -48,11 +56,15 @@ export default function CookieBanner() {
                     </p>
                 </Link>
             </div>
-        
+    
             <div className='flex gap-2'>
-                <button className='px-5 py-2 text-white rounded-md border-blue-888' onClick={handleDecline}>Decline</button>
-                <button className='bg-blue-600 px-5 py-2 text-white rounded-lg' onClick={handleAllowCookies}>Allow Cookies</button>
+                {/* <button className='px-5 py-2 text-white rounded-md border-blue-888' onClick={() => setCookieConsent(false)}>Decline</button>
+                <button className='bg-blue-600 px-5 py-2 text-white rounded-lg' onClick={() => setCookieConsent(true)}>Allow Cookies</button> */}
+                 <button className='px-5 py-2 text-white rounded-md border-blue-888' onClick={handleDecline}>Decline</button>
+                <button className='bg-blue-600 px-5 py-2 text-white rounded-lg' onClick={handleAllowCookies}>Allow Cookies</button>  
             </div>
         </div>
-    ) : null;
+    );
+    
+    
 }
