31cfe6a21b7e6dba9c372fe9ed4a96dd7a5f5b8c
totobece <savaltobias5@gmail.com>
2024-01-30 18:00:07 -0300

    GTM WORKING!!!

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,13 +27,30 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={bebas_neue.className}>
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-K7N66L4G')`}}
+        
+        />
+      </head>
       <GoogleAnalytics GA_MEASUREMENT_ID='G-FRPLFF3P4W'/>
       <body className={` bg-zinc-50	 text-blue-888 tracking-tight`}>
+
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K7N66L4G"
+          height="0" width="0" ></iframe>
+
         <div className="flex flex-col min-h-screen overflow-hidden">
           <Header />
           {children}
           <Banner />
         </div>
+
+        
       </body>
     </html>
     
