90757acfd98d196d434913f0dfc52f49745a24de
totobece <savaltobias5@gmail.com>
2024-02-04 20:49:23 -0300

    gtm working in production

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import Header from '@/components/ui/header'
 import Banner from '@/components/banner'
 import { Metadata } from 'next'
 import GoogleAnalytics from '@/components/GoogleAnalytics'
+import {GoogleTagManager} from '@next/third-parties/google'
 
 
 const bebas_neue = Bebas_Neue({
@@ -27,22 +28,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={bebas_neue.className}>
-      <head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-K7N66L4G')`}}
-        
-        />
-      </head>
+   
       <GoogleAnalytics GA_MEASUREMENT_ID='G-FRPLFF3P4W'/>
       <body className={` bg-zinc-50	 text-blue-888 tracking-tight`}>
-
-        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K7N66L4G"
-          height="0" width="0" ></iframe>
 
         <div className="flex flex-col min-h-screen overflow-hidden">
           <Header />
@@ -52,7 +40,9 @@ export default function RootLayout({
 
         
       </body>
+      <GoogleTagManager gtmId='GTM-K7N66L4G'/>
     </html>
+    
     
     )
   }
