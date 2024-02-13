8a56698d3abfbb0d186631ae264f0b5aefb19523
GitButler <gitbutler@gitbutler.com>
2024-02-13 15:57:52 -0300

    GitButler Integration Commit
    This is an integration commit for the virtual branches that GitButler is tracking.
    
    Due to GitButler managing multiple virtual branches, you cannot switch back and
    forth between git branches and virtual branches easily. 
    
    If you switch to another branch, GitButler will need to be reinitialized.
    If you commit on this branch, GitButler will throw it away.
    
    Here are the branches that are currently applied:
     - features (refs/gitbutler/features)
       branch head: 6771164dce039325580169a41cbc40082d8d6336
       - components/ui/header.tsx
       - package-lock.json
       - pages/_app.tsx
       - components/CookieBanner.tsx
       - app/servicios/page.tsx
       - components/ui/mobile-menu.tsx
       - components/zigzag.tsx
       - package.json
       - components/hero.tsx
       - app/nosotros/page.tsx
       - public/images/Free whatsapp blue logo vector.svg-svg.svg
       - public/images/IBM_logo.svg.png
       - public/images/SaucotecLogo-white-svg.svg
       - tailwind.config.js
       - app/clientes/page.tsx
       - .vscode/settings.json
       - app/css/style.css
       - public/images/Microsoft_logo_(2012).svg.png
       - public/images/640px-Apple-logo.png
    
    Your previous branch was: refs/heads/features
    
    The sha for that commit was: 6771164dce039325580169a41cbc40082d8d6336
    
    For more information about what we're doing here, check out our docs:
    https://docs.gitbutler.com/features/virtual-branches/integration-branch

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,6 @@ import Header from '@/components/ui/header'
 import Banner from '@/components/banner'
 import { Metadata } from 'next'
 import GoogleAnalytics from '@/components/GoogleAnalytics'
-import {GoogleTagManager} from '@next/third-parties/google'
 
 
 const bebas_neue = Bebas_Neue({
@@ -15,6 +14,13 @@ const bebas_neue = Bebas_Neue({
   variable: '--font-bebas-neue',
   display: 'swap'
 })
+
+/* const bebas_neue = bebas_neue({
+  subsets: ['latin'],
+  variable: '--bebas_neue',
+  weight: '400',
+  display: 'swap'
+}) */
 
 export const metadata : Metadata = {
   title: 'SaucoTec',
@@ -28,23 +34,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={bebas_neue.className}>
-   
-      <GoogleAnalytics GA_MEASUREMENT_ID='G-FRPLFF3P4W'/>
+      <GoogleAnalytics GA_MEASUREMENT_ID='AW-11432082618'/>
       <body className={` bg-zinc-50	 text-blue-888 tracking-tight`}>
-
         <div className="flex flex-col min-h-screen overflow-hidden">
           <Header />
           {children}
           <Banner />
         </div>
-
-        
       </body>
-      <GoogleTagManager gtmId='GTM-K7N66L4G'/>
     </html>
     
-    
-    )
-  }
-  
-  /* (${architects_daughter.variable} ) */
+  )
+}
+ 
+/* (${architects_daughter.variable} ) */
--- lib/gtagHelper.ts
+++ lib/gtagHelper.ts
@@ -1,26 +1,18 @@
 // Extiende la interfaz Window
 declare global {
     interface Window {
-        FB?: any;
-        gtag?: GtagFunction;
+        FB: any;
+        gtag: (event: string, action: string, options?: any) => void;
     }
 }
 
-// Define el tipo para la función gtag
-type GtagFunction = (event: string, action: string, options?: Record<string, any>) => void;
-
 // Accede a las propiedades extendidas
-let FB: any;
-let gtag: GtagFunction | undefined;
-
-if (typeof window !== 'undefined') {
-    FB = window.FB;
-    gtag = window.gtag;
-}
+let FB = window.FB;
+let gtag = window.gtag;
 
 // Exporta la función con la corrección
 export const pageview = (GA_MEASUREMENT_ID: string, url: string) => {
-    if (typeof window !== 'undefined' && gtag) {
+    if (typeof window !== 'undefined') {
         // Utiliza la función gtag
         gtag("config", GA_MEASUREMENT_ID, {
             page_path: url,
