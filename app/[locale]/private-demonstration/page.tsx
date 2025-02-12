4d9213db4f001efd32cd7af6f7b21ef46b07a8c5
Pedro Bergaglio <pedrobergaglio04@gmail.com>
2025-02-11 21:22:45 -0300

    better mobile formatting

--- app/[locale]/private-demonstration/page.tsx
+++ app/[locale]/private-demonstration/page.tsx
@@ -1,9 +1,12 @@
+import styles from './styles.module.css';
+
 export default function Page() {
   return (
-    <iframe 
-      src="https://www.appsheet.com/start/58f35c5f-0c82-4960-8a48-a2c1e0d45f52?refresh=1&wipe=1"
-      width="100%"
-      style={{border: "none", height: '100vh'}}
-    ></iframe>
+    <div className={styles.container}>
+      <iframe 
+        src="https://www.appsheet.com/start/58f35c5f-0c82-4960-8a48-a2c1e0d45f52?refresh=1&wipe=1"
+        className={styles.iframe}
+      ></iframe>
+    </div>
   );
 }
